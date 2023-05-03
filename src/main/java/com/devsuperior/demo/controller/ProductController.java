9d5844e1acbd0d2342b0f117367aa571644c3cc3
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-05-03 17:51:02 -0400

    update productcontroller

--- src/main/java/com/devsuperior/demo/controller/ProductController.java
+++ src/main/java/com/devsuperior/demo/controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.demo.entities.Department;
 import com.devsuperior.demo.entities.Product;
 import com.devsuperior.demo.repositories.ProductRepository;
 
